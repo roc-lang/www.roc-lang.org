c821214b0a0ca77f49f5b51d719d5e91a46d9385
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-07-18 12:25:30 +0200

    add build_cloudflare.sh

---
+++ build_cloudflare.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+# Download latest Roc nightly release
+curl -fOL https://github.com/roc-lang/roc/releases/download/nightly/roc_nightly-linux_x86_64-latest.tar.gz
+
+# rename nightly tar
+mv $(ls | grep "roc_nightly.*tar\.gz") roc_nightly.tar.gz
+
+# decompress the tar
+tar -xzf roc_nightly.tar.gz
+
+rm roc_nightly.tar.gz
+
+# simplify nightly folder name
+mv roc_nightly* roc_nightly
+
+./roc_nightly/roc version
+
+export PATH=$PATH:$(pwd)/roc_nightly
+
+cd website
+roc check build_website.roc
+roc build build_website.roc
+./build_website
